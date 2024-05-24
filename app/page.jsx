ef8778dd71f8d45a01b685665757a46f83aacd95
shane <shane@extension-interactive.com>
2024-05-24 10:33:25 +0400

    init about mea

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Hero from "@/src/components/hero/Hero";
+import MeaAbout from "@/src/components/mea_about/MeaAbout";
 
 export default function Home() {
   return (
     <main>
-      <Hero></Hero>
+      <Hero/>
+      <MeaAbout/>
     </main>
   );
 }
