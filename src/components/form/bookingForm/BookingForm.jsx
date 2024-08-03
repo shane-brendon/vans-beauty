87db3567606f260d4e3aeb1bfb0b333cfbdef525
shane <rshane.dev@gmail.com>
2024-08-03 22:45:30 +0400

    booking

---
+++ src/components/form/bookingForm/BookingForm.jsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import styles from "./bookingForm.module.scss";
+import { useForm } from "react-hook-form";
+function BookingForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <form className={styles.bookingForm}>
+      
+    </form>
+  );
+}
+
+export default BookingForm;
