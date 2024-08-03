02a5813cd8b099a37ab34adad3bbfbbebbf84e98
shane <rshane.dev@gmail.com>
2024-08-03 23:15:15 +0400

    convert

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-import path from 'path'; // Add this line to import the 'path' module
-
-const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
